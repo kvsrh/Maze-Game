66d0fa29ceb6fd91f4bc5861940b2a2326e634d1
Hareen Kancharla <kancharlahareen@gmail.com>
2025-04-18 12:13:34 -0700

    Difficulty level 10 -> 20

--- script.js
+++ script.js
@@ -597,7 +597,7 @@ function rand(max) {
     // <option value="15">Medium</option>
     // <option value="25">Hard</option>
     // <option value="38">Extreme</option>   
-    difficulty = 10;
+    difficulty = 20;
 
     cellSize = mazeCanvas.width / difficulty;
     maze = new Maze(difficulty, difficulty);
